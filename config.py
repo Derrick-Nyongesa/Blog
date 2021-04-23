8f3bc5959811837c2145ae82f8ee88714a106ebf
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-04-23 16:54:47 +0300

    configured photo uploads and created a profile pic update view

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 class ProdConfig(Config):
     pass
 
