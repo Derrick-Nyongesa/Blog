ecb64ff29ae5911dd103be4a33b5c42902313fd9
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-04-23 12:05:12 +0300

    created and registered blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -16,4 +16,9 @@ app.config.from_object(config_options[config_name])
 bootstrap.init_app(app)
 
 
+# Registering the blueprint
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
+
+
 return app
