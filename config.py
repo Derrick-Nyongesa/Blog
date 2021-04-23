0a5f7b0b1493e19ae463e1112a69bb6fcb982d6f
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-04-23 11:51:43 +0300

    application configurations

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+class Config:
+    pass
+
+class ProdConfig:
+    pass
+
+class DevConfig:
+    DEBUG = True
+
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
