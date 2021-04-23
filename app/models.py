1964a3752866e5b587658c3604fc0b4a21c700e0
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-04-23 14:20:55 +0300

    added comments model

--- app/models.py
+++ app/models.py
@@ -4,6 +4,19 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    comments = db.relationship('Comment',backref = 'role',lazy="dynamic")
 
     def __repr__(self):
         return f'User {self.username}'
+
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String(255))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def __repr__(self):
+        return f'User {self.comment}'
