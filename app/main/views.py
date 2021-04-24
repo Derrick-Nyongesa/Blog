c1b99a90de7bc15e3310b84310e09339fdafc0b9
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-04-24 08:40:50 +0300

    implemented template inheritance

--- app/main/views.py
+++ app/main/views.py
@@ -35,9 +35,9 @@ def pitch():
 
 @main.route('/details/<int:id>')
 def details(id):
-    details = Pitch.get_pitch(id)
+    pitch = Pitch.get_pitch(id)
 
-    return render_template('details.html', details = details)
+    return render_template('details.html', pitch = pitch)
 
 
 
