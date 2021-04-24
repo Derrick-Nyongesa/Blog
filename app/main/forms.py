6c24806b65d5e16ea3c4bac4178f323eccca73cb
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-04-24 09:24:26 +0300

    updated the comment form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField,SelectField
-from wtforms.validators import Required,DataRequired,EqualTo,EqualTo
-from ..models import Pitch
+from wtforms.validators import Required,DataRequired,Email,EqualTo
+from ..models import Pitch,Comment
 
 
 class UpdateProfile(FlaskForm):
@@ -13,4 +13,10 @@ class NewPitch(FlaskForm):
     category = SelectField('Pitch Category', coerce=int, choices=[(0, 'Please Input your Category...'), (1, 'Interview'),(2, 'pickup_lines'),(3, 'product_pitch'),(4, 'promotion_pitch')],validators=[DataRequired()])
     title = StringField('Pitch Title', validators=[Required()])
     pitch = TextAreaField('Pitch',validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class CommentForm(FlaskForm):
+    title = StringField('Pitch title', validators=[Required()])
+    review = TextAreaField('Comment',)
     submit = SubmitField('Submit')
